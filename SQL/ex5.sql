95c8e3cb8ae00fcfef464738b588a668c25dcf46
Thanusan Ramanasingam <tramanas@uwo.ca>
2025-11-21 21:53:38 -0500

    finished all 7 queries for question 5

--- SQL/ex5.sql
+++ SQL/ex5.sql
@@ -1,18 +1,21 @@
+-- query 1
 select *
 from bet
 where amount>200;
 
+-- query 2
 SELECT playerEmail, sum(amount) AS total_winnings
 from bet
 where outcome=1
 group by playerEmail;
 
-
+-- query 3
 SELECT username,Balance,reason,endDate
 FROM player
 JOIN bans ON bans.playerEmail = player.playerEmail
 WHERE balance > 0;
 
+-- query 4
 SELECT *
 FROM bet
 WHERE amount > (
@@ -20,6 +23,7 @@ WHERE amount > (
     FROM bet
 );
 
+-- query 5
 SELECT *
 FROM player
 WHERE NOT EXISTS (
@@ -28,9 +32,20 @@ WHERE NOT EXISTS (
     WHERE bans.playerEmail = player.playerEmail
 );
 
+-- query 6
 SELECT player.playerEmail,
        COUNT(dateTimePlaced) AS bet_placed
 FROM Player
 JOIN bet ON bet.playerEmail = player.playerEmail
 GROUP BY player.playerEmail
 ORDER BY bet_placed DESC;
+
+-- query 7
+SELECT player.playerEmail, bet.amount
+FROM player
+JOIN bet ON player.playerEmail = bet.playerEmail
+WHERE bet.amount IN (
+    SELECT amount
+    FROM bet
+    WHERE amount > 400
+);
